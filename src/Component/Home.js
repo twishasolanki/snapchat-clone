e718447166ffeb8fcf280423a4ddff2ddc807584
twishasolanki <twishasolanki3003@gmail.com>
2023-09-18 08:08:53 +0530

    initial commint

---
+++ src/Component/Home.js
@@ -0,0 +1,194 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <div className="grid grid-cols-6 gap-4">
+        <div className="col-start-2 col-span-4 ...">
+        <div className='bg-black m-48 mt-2 container' style={{ width: "55%" }}>
+        <div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"4px",height:"50px",width:"50px"}}>
+        <div>
+          <img src="av1.jpg" style={{height:"45px",width:"45px",marginTop:"-0px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    
+        <div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"80px",height:"45px",width:"45px",marginTop:"20px"}}>
+        <div>
+          <img src="search.jpg" style={{height:"40px",width:"40px",marginLeft:"1px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+          <h4 class="text-2xl font-bold text-white" style={{marginTop:"-35px",marginLeft:"130px"}}>Chat</h4>
+          <img src="add.png" style={{height:"40px",width:"40px",marginLeft:"300px",marginTop:"-36px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+          <img src="dot.jpg" style={{height:"40px",width:"40px",marginLeft:"360px",marginTop:"-39px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+      
+        <div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"10px",height:"40px",width:"60px",marginTop:"100px"}}>
+          <img src="chat.png" style={{height:"36px",width:"53px",marginTop:"1px",marginLeft:"2px"}} className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        <div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"90px",height:"40px",width:"60px",marginTop:"100px"}}>
+        <img src="contact.png" style={{height:"36px",width:"53px",marginTop:"1px",marginLeft:"2px"}} className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        <div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"170px",height:"40px",width:"60px",marginTop:"100px"}}>
+        <img src="conn.jpg" style={{height:"36px",width:"53px",marginTop:"1px",marginLeft:"2px"}} className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        <div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"250px",height:"40px",width:"60px",marginTop:"100px"}}>
+        <img src="smiley.jpg" style={{height:"36px",width:"53px",marginTop:"1px",marginLeft:"2px"}} className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        <div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"330px",height:"40px",width:"60px",marginTop:"100px"}}>
+        <img src="smile1.jpg" style={{height:"36px",width:"53px",marginTop:"1px",marginLeft:"2px"}} className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        <div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"410px",height:"40px",width:"60px",marginTop:"100px"}}>
+        <img src="plus1.png" style={{height:"36px",width:"53px",marginTop:"1px",marginLeft:"2px"}} className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        
+        <div className="flex flex-wrap justify-center"></div>
+
+        <hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"160px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+  
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av1.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered </p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.2m</p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+
+<hr className="h-px  bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+ 
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av5.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered</p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.1m </p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+ 
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av1.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered</p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.40m </p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+  
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av4.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered</p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.12m </p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+ 
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av6.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered </p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.15m</p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+ 
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av1.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered </p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.30m</p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+ 
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av2.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered </p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.7m</p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av4.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered </p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.10m</p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+ 
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av3.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered</p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.2m</p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+ 
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av6.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered </p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.3d</p>
+<img src="camera2.png" style={{height:"40px",width:"40px",marginLeft:"400px",marginTop:"50px"}}/>
+</div>
+
+<hr className="h-px mt-1 bg-gray-100 border-0 dark:bg-gray-100" style={{marginTop:"100px"}}/>   
+  <div class="absolute rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{width:"80px",height:"80px",marginLeft:"10px"}}>
+ 
+<div class="absolute h-20 w-20 rounded-full bg-gray-600 border-2 border-gray-600 mt-4" style={{marginLeft:"1px",height:"60px",width:"75px"}}>
+        <div>
+          <img src="av5.jpg" style={{height:"80px",width:"90px",marginTop:"-20px"}} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+        </div>
+    <p className="text-white p-10 font-bold" style={{marginLeft:"50px",marginTop:"-30px"}} >Twisha</p>  
+<img src="icon1.jpg" style={{marginTop:"-40px",marginLeft:"90px", height:"30px"}} /><p className='text-white' style={{marginLeft:"130px",marginTop:"-25px"}}> Delivered </p><p className='text-white' style={{marginLeft:"210px",marginTop:"-23px"}}>.3d</p>
+
+</div>
+        <br />   <br /> <br /> <br /> <br />   Home  <br />     Home  <br />     Home  <br />     Home  <br />     Home  <br />     Home    </div>
+
+</div></div>
+         </>
+  )
+}
